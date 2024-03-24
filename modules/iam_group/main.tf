4974ca193e5f417e154f5f9ec1d7bd2cf0738114
harshpreet <149198714+harshpreetkhokhar@users.noreply.github.com>
2024-03-24 10:58:50 +0530

    Update main.tf

--- modules/iam_group/main.tf
+++ modules/iam_group/main.tf
@@ -12,7 +12,6 @@ resource "aws_iam_group_policy_attachment" "group_policy_attachment" {
 
 resource "aws_iam_group_membership" "group_membership" {
   count = length(var.user_names)
-
   name  = "group_membership_${var.group_name}"
   users = [var.user_names[count.index]]
   group = aws_iam_group.this.name
