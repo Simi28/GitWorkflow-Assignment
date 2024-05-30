df0790ef0e86501c83482fed5858050b141900c9
Vaibhav Mukheja <79351721+muk679@users.noreply.github.com>
2024-05-30 21:47:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
  terraform { 
   backend "s3" {
-    bucket         	   = "vaibhav-123"
+    bucket         	   = "vaibhav-1234"
     key                = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
